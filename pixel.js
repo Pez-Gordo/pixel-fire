6c1b112ad96f5ce60ee9aefabdb13ec5cef87292
pez <emvenegas@gmail.com>
2021-03-25 00:45:31 +0000

    diagonal rain effect

--- pixel.js
+++ pixel.js
@@ -875,7 +875,8 @@ myImage.addEventListener('load', function() {
             const blue = pixel.data[(y * 4 * pixel.width) + (x * 4 + 2)]
             const brightness = calculateRelativeBrightness(red, green, blue)
             const cell = [
-                cellBrightness = brightness
+                cellBrightness = brightness,
+                cellColor = 'rgb(' + red + ',' + green + ',' + blue + ')'
             ]
             row.push(cell)
         }
@@ -909,14 +910,19 @@ myImage.addEventListener('load', function() {
             let movement = (2.5 - this.speed) + this.velocity
 
             this.y += movement
+            this.x += movement
             if (this.y >= canvas.height) {
                 this.y = 0
                 this.x = Math.random() * canvas.width
             }
+            if (this.x >= canvas.width) {
+                this.x = 0
+                this.y = Math.random() * canvas.height
+            }
         }
         draw() {
             ctx.beginPath()
-            ctx.fillStyle = 'white'
+            ctx.fillStyle = mappedImage[this.position1][this.position2][1]
             ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
             ctx.fill()
         }
