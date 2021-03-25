2e2972a9852ef284c7bb326a43fe304ec6f82543
pez <emvenegas@gmail.com>
2021-03-25 02:01:06 +0000

    exercise finished

--- pixel.js
+++ pixel.js
@@ -858,13 +858,31 @@ myImage.addEventListener('load', function() {
     const ctx = canvas.getContext('2d')
     canvas.width = 330
     canvas.height = 468
+    const gradient1 = ctx.createLinearGradient(0, 0, canvas.width, canvas.height)
+    gradient1.addColorStop(0.2,'pink')
+    gradient1.addColorStop(0.3,'red')
+    gradient1.addColorStop(0.4,'orange')
+    gradient1.addColorStop(0.5,'yellow')
+    gradient1.addColorStop(0.6,'green')
+    gradient1.addColorStop(0.7,'turquoise')
+    gradient1.addColorStop(0.8,'violet')
+
+    const letters = ['M', 'A', 'D']
+    let switcher = 1
+    let counter = 0
+    setInterval(function() {
+        counter++
+        if (counter % 12 === 0) {
+            switcher *= -1
+        }
+    }, 500)
     
     ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height)
     const pixel = ctx.getImageData(0, 0, canvas.width, canvas.height)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     let particlesArray = []
-    const numberOfParticles = 2500
+    const numberOfParticles = 850
 
     let mappedImage = []
     for (let y = 0; y < canvas.height; y++) {
@@ -898,21 +916,39 @@ myImage.addEventListener('load', function() {
             this.y = 0
             this.speed = 0
             this.velocity = Math.random() * 0.5
-            this.size = Math.random() * 1.5 + 1
+            this.size = Math.random() * 2.5 + 0.2
             this.position1 = Math.floor(this.y)
             this.position2 = Math.floor(this.x)
-
+            this.angle = 0
+            this.letter = letters[Math.floor(Math.random() * letters.length)]
         }
         update() {
             this.position1 = Math.floor(this.y)
             this.position2 = Math.floor(this.x)
-            this.speed = mappedImage[this.position1][this.position2][0]
-            let movement = (2.5 - this.speed) + this.velocity
 
-            this.y += movement
-            this.x += movement
-            if (this.y >= canvas.height) {
+            if((mappedImage[this.position1])&&(mappedImage[this.position1][this.position2])) {
+                this.speed = mappedImage[this.position1][this.position2][0]
+            }
+            let movement = (2.5 - this.speed) + this.velocity
+            this.angle += this.speed / 20
+            this.size = this.speed * 0.5
+            /*
+            if (switcher === 1) {
+                ctx.globalCompositeOperator = 'luminosity'
+            } else {
+                ctx.globalCompositeOperator = 'soft-light'
+            }
+
+            if (counter % 10 === 0) {
+                this.x = Math.random() * canvas.width
                 this.y = 0
+            }
+            */
+
+            this.y -= movement
+            this.x += movement + Math.sin(this.angle) * 2
+            if (this.y <= 0) {
+                this.y = canvas.height
                 this.x = Math.random() * canvas.width
             }
             if (this.x >= canvas.width) {
@@ -922,7 +958,16 @@ myImage.addEventListener('load', function() {
         }
         draw() {
             ctx.beginPath()
-            ctx.fillStyle = mappedImage[this.position1][this.position2][1]
+            
+            if((mappedImage[this.position1])&&(mappedImage[this.position1][this.position2])) {
+                ctx.fillStyle = mappedImage[this.position1][this.position2][1]
+                ctx.strokeStyle = mappedImage[this.position1][this.position2][1]
+            }
+            
+            //ctx.fillStyle = gradient1
+            //ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+            //ctx.strokeRect(this.x, this.y, this.size * 3, this.size * 3)
+            ctx.font = '20px Arial'
             ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
             ctx.fill()
         }
@@ -942,7 +987,8 @@ myImage.addEventListener('load', function() {
         globalAlpha = 0.2
         for (let i = 0; i < particlesArray.length; i++) {
             particlesArray[i].update()
-            ctx.globalAlpha = particlesArray[i].speed * 0.5
+            //ctx.globalAlpha = particlesArray[i].speed * 0.5
+            ctx.globalAlpha = 1
             particlesArray[i].draw()
         }
         requestAnimationFrame(animate)
